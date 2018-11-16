6edb9a5caf288ca78f7f7c6e427c79fe200c67b7
truyenho95 <truyenho.webdev@gmail.com>
2018-11-16 19:06:59 +0700

    Update style.css Mobile.js

--- js/Mobile.js
+++ js/Mobile.js
@@ -10,24 +10,30 @@ function Mobile(namePhone, phoneNumber) {
   this.power = () => {
     this.isTurnOn = (this.isTurnOn) ? false : true;
     if (this.isTurnOn) {
-      const rowCreateMessengeSelector = document.querySelectorAll(`div#${namePhone} table tr.createMessenge`).forEach(item => {
-        item.style.display = '';
-      });
-      const rowMessBoxSelector = document.querySelectorAll(`div#${namePhone} table tr.messBox`).forEach(item => {
-        item.style.display = '';
-      });
+      this.turnOn();
     }
     else {
-      const rowCreateMessengeSelector = document.querySelectorAll(`div#${namePhone} table tr.createMessenge`).forEach(item => {
-        item.style.display = 'none';
-      });
-      const rowMessBoxSelector = document.querySelectorAll(`div#${namePhone} table tr.messBox`).forEach(item => {
-        item.style.display = 'none';
-      });
+      this.turnOff();
     }
   }
   this.showTurnOnStatus = () => {
     const turnOnStatus = document.getElementById(`${namePhone}IsTurnOn`);
     turnOnStatus.innerHTML = (this.isTurnOn) ? '<b>ON</b>' : '<b>OFF</b>';
+  }
+  this.turnOn = () => {
+    const rowCreateMessengeSelector = document.querySelectorAll(`div#${namePhone} table tr.createMessenge`).forEach(item => {
+      item.style.display = '';
+    });
+    const rowMessBoxSelector = document.querySelectorAll(`div#${namePhone} table tr.messBox`).forEach(item => {
+      item.style.display = '';
+    });
+  }
+  this.turnOff = () => {
+    const rowCreateMessengeSelector = document.querySelectorAll(`div#${namePhone} table tr.createMessenge`).forEach(item => {
+      item.style.display = 'none';
+    });
+    const rowMessBoxSelector = document.querySelectorAll(`div#${namePhone} table tr.messBox`).forEach(item => {
+      item.style.display = 'none';
+    });
   }
 }
